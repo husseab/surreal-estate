9832eb0138ede38ea02c0cccc2e0c3cff2a33406
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-11-27 19:22:28 +0000

    route setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "../styles/app.css";
+import AddProperty from "./AddProperty";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
 
 const App = () => {
   return (
     <div className="App">
       <h2>Surreal Estate</h2>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="/add-property" element={<AddProperty />} />
+      </Routes>
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 
 const NavBar = () => {
@@ -9,8 +10,16 @@ const NavBar = () => {
         alt="logo"
       />
       <ul className="navbar-links">
-        <li className="navbar-links-item">View Properties</li>
-        <li className="navbar-links-item">Add a property</li>
+        <li className="navbar-links-item">
+          <Link className="item" to="/">
+            View properties
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="item" to="/add-property">
+            Add property
+          </Link>
+        </li>
       </ul>
     </div>
   );
