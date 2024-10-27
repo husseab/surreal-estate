1219d2913c70ac0fe8c15ab25f9e64b62f589eb6
husseab <abdikarim.hussein557@gmail.com>
2024-10-27 19:49:28 +0000

    updates on nav and view properties

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -6,42 +6,57 @@ const NavBar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <div className="navbar-brand">
-        <Link to={"/"}>
-          <img
-            src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
-            alt="logo"
-            style={{ height: "100px" }}
-          />
-        </Link>
-      </div>
-      <div className="navbar-nav">
-        <div className="nav-item nav-link">
-          <Link
-            to="/view-properties"
-            style={{
-              textDecoration: "none",
-              color: hover === "view-properties" ? "blue" : "black",
-            }}
-            onMouseEnter={() => setHover("view-properties")}
-            onMouseLeave={() => setHover(false)}
-          >
-            View properties
+      <div className="container-fluid">
+        {" "}
+        {/* Container for proper spacing */}
+        <div className="navbar-brand">
+          <Link to={"/"}>
+            <img
+              src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
+              alt="logo"
+              style={{ height: "100px" }}
+            />
           </Link>
-          <span> &nbsp;</span>
         </div>
-        <div className="nav-item nav-link">
-          <Link
-            to="/add-property"
-            style={{
-              textDecoration: "none",
-              color: hover === "add-property" ? "blue" : "black",
-            }}
-            onMouseEnter={() => setHover("add-property")}
-            onMouseLeave={() => setHover(false)}
+        <div className="navbar-nav me-auto">
+          <div className="nav-item nav-link">
+            <Link
+              to="/view-properties"
+              style={{
+                textDecoration: "none",
+                color: hover === "view-properties" ? "blue" : "black",
+              }}
+              onMouseEnter={() => setHover("view-properties")}
+              onMouseLeave={() => setHover(false)}
+            >
+              View properties
+            </Link>
+            <span> &nbsp;</span>
+          </div>
+          <div className="nav-item nav-link">
+            <Link
+              to="/add-property"
+              style={{
+                textDecoration: "none",
+                color: hover === "add-property" ? "blue" : "black",
+              }}
+              onMouseEnter={() => setHover("add-property")}
+              onMouseLeave={() => setHover(false)}
+            >
+              Add property
+            </Link>
+          </div>
+        </div>
+        <div className="d-flex align-items-center me-5">
+          {" "}
+          {/* Flex container for alignment */}
+          <span
+            className="material-icons"
+            style={{ fontSize: "30px", marginRight: "5px" }}
           >
-            Add property
-          </Link>
+            account_circle
+          </span>
+          <span className="navbar-text">John Smith</span>
         </div>
       </div>
     </nav>
--- src/components/view_properties/ViewProperties.js
+++ src/components/view_properties/ViewProperties.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { Alert } from "antd";
 import {
   getPropertyListing,
   // getImage,
   // getPropertyCount,
 } from "../../service/Properties.service";
-import { Link } from "react-router-dom";
 // import { Spin, Alert } from "antd";
 
 const Properties = () => {
   const [propertiesList, setPropertiesList] = useState([]);
+  const [alertMessage, setAlertMessage] = useState("");
   // const [imageUrls, setImageUrls] = useState([]);
   // const [propertiesCount, setPropertiesCount] = useState(0);
 
@@ -16,8 +17,13 @@ const Properties = () => {
     (async () => {
       // const totalCount = await getPropertyCount();
       const properties = await getPropertyListing();
-      if (properties.length > 0) {
+      // const properties = undefined
+      if (properties && properties.length > 0) {
         setPropertiesList(properties);
+      } else {
+        setAlertMessage(
+          "Error: Unable to retrieve properties. Server not available."
+        );
       }
       // if (totalCount?.count) {
       //   setPropertiesCount(totalCount.count);
@@ -47,45 +53,49 @@ const Properties = () => {
       ) : ( */}
       <div className="container mt-4">
         <div className="row g-4">
-          {propertiesList.length > 0
-            ? propertiesList.map((item, index) => {
-                return (
-                  <div key={item._id} className="col-6 col-md-4 col-lg-2">
-                    <div className="card h-100">
-                      <img
-                        src={`https://picsum.photos/300/300?random=${index}`}
-                        className="card-img-top"
-                        alt="city"
-                      />
-                      <div className="card-body d-flex flex-column">
-                        <h5 className="card-title">{item.title}</h5>
-                        <p className="card-text mb-3 flex-grow-1">
-                          {
-                            "Surreal Estate propeties are mock properties to test features."
-                          }
-                        </p>
-                      </div>
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item">City: {item.city}</li>
-                        <li className="list-group-item">Type: {item.type}</li>
-                        <li className="list-group-item">
-                          Bedroooms: {item.bedrooms}
-                        </li>
-                        <li className="list-group-item">
-                          Bathrooms: {item.bathrooms}
-                        </li>
-                        <li className="list-group-item">Price: {item.price}</li>
-                      </ul>
-                      <div className="card-body">
-                        {/* <Link to="/" className="card-link">
-                          View Property
-                        </Link> */}
-                      </div>
+          {alertMessage ? (
+            <Alert type="error" message={alertMessage || "Error"} />
+          ) : propertiesList && propertiesList.length > 0 ? (
+            propertiesList.map((item, index) => {
+              return (
+                <div key={item._id} className="col-6 col-md-4 col-lg-2">
+                  <div className="card h-100">
+                    <img
+                      src={`https://picsum.photos/300/300?random=${index}`}
+                      className="card-img-top"
+                      alt="city"
+                    />
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">{item.title}</h5>
+                      <p className="card-text mb-3 flex-grow-1">
+                        {
+                          "Surreal Estate propeties are mock properties to test features."
+                        }
+                      </p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">City: {item.city}</li>
+                      <li className="list-group-item">Type: {item.type}</li>
+                      <li className="list-group-item">
+                        Bedroooms: {item.bedrooms}
+                      </li>
+                      <li className="list-group-item">
+                        Bathrooms: {item.bathrooms}
+                      </li>
+                      <li className="list-group-item">Price: {item.price}</li>
+                    </ul>
+                    <div className="card-body">
+                      {/* <Link to="/" className="card-link">
+                              View Property
+                            </Link> */}
                     </div>
                   </div>
-                );
-              })
-            : ""}
+                </div>
+              );
+            })
+          ) : (
+            ""
+          )}
         </div>
       </div>
       {/* )} */}
